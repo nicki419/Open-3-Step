f09d7821527f82cd4e07546e7ace59630573adb5
Nick <niklas.braun30419@gmail.com>
2025-08-31 16:24:14 +0200

    Implement SettingsController and EdgeDetector (#5)
    * Implement SettingsController
    
    * Read brightness from eeprom when cycling
    
    * Changed Delay to 500ms in a definition
    
    * Added extra config to platformio.ini

--- Code/v_0.2/include/brightness_controller.h
+++ Code/v_0.2/include/brightness_controller.h
@@ -7,16 +7,17 @@ class BrightnessController {
 private:
     int brightnessLevels[3] = {30, 70, 100};
     int currentBrightnessIndex = 0;
+#ifdef DEBUG
     bool ledOn = false;
     unsigned long lastBlinkTime = 0;
+#endif
 
     dimmerLamp* dimmer;
     AT24C256* eeprom;
     
 public:
     BrightnessController(dimmerLamp* dimmer, AT24C256* eeprom);
-    
-    // Public methods
+
     void applyBrightness() const;
     void cycleBrightness();
 
@@ -28,11 +29,15 @@ public:
 
     [[nodiscard]] bool writeAndVerify(byte address, byte data) const;
     [[nodiscard]] byte readWithRetry(byte address, byte defaultValue) const;
-    void saveBrightness(int currentBrightnessIndex) const;
+    [[nodiscard]] byte readBrightnessWithRetry(byte address, byte defaultValue) const;
+    void saveBrightnessIndex(int currentBrightnessIndex) const;
     
     // Getters
     [[nodiscard]] int getCurrentBrightness() const;
     [[nodiscard]] int getCurrentBrightnessIndex() const;
+
+    // Setters
+    void setCurrentBrightnessIndex(int currentBrightnessIndex);
 };
 
 #endif
